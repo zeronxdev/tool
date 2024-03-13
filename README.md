4d051c8b371898f7ac0fadcdead2b740d3d35c40
ZeronX <160329349+zeronxdev@users.noreply.github.com>
2024-03-13 10:25:41 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# tool
-Tool Linux
+◄▬▬▬▬𝐂𝐎𝐏𝐘𝐑𝐈𝐆𝐇𝐓©𝐇𝐓𝟒𝐆𝐕𝐏𝐍▬▬▬▬►
+# Tool aaPanel By HT4GVPN
+```
+bash <(curl -Ls https://raw.githubusercontent.com/zeronxdev/tool/main/run.sh)
+```
+◄▬▬▬▬𝐂𝐎𝐏𝐘𝐑𝐈𝐆𝐇𝐓©𝐇𝐓𝟒𝐆𝐕𝐏𝐍▬▬▬▬►
---
+++ crack.sh
@@ -0,0 +1,21 @@
+echo "Đang Crack, vui lòng chờ"
+sed -i 's|"endtime": -1|"endtime": 999999999999|g' /www/server/panel/data/plugin.json
+sed -i 's|"pro": -1|"pro": 0|g' /www/server/panel/data/plugin.json
+chattr +i /www/server/panel/data/plugin.json
+chattr -i /www/server/panel/data/repair.json
+rm /www/server/panel/data/repair.json
+cd /www/server/panel/data
+wget https://raw.githubusercontent.com/zeronxdev/tool/main/data/repair.json
+chattr +i /www/server/panel/data/repair.json
+rm /www/server/panel/data/plugin.json
+cd /www/server/panel/data
+wget https://raw.githubusercontent.com/zeronxdev/tool/main/data/plugin.json
+chattr +i /www/server/panel/data/plugin.json
+rm /www/server/panel/BTPanel/static/js/soft.js
+cd /www/server/panel/BTPanel/static/js
+wget https://raw.githubusercontent.com/zeronxdev/tool/main/data/soft.js
+chattr +i /www/server/panel/data/soft.js
+rm /www/server/panel/BTPanel/static/js/index.js
+cd /www/server/panel/BTPanel/static/js
+wget https://raw.githubusercontent.com/zeronxdev/tool/main/data/index.js
+chattr +i /www/server/panel/BTPanel/static/js/index.js
